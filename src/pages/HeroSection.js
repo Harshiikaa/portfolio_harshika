eacd8146fc3702705717b8b74f0890991b379601
Harshiikaa <chaudharyharshika8@gmail.com>
2025-10-17 18:24:55 +0545

    changed the position of the model

--- src/pages/HeroSection.js
+++ src/pages/HeroSection.js
@@ -30,22 +30,22 @@ function Sculpture({ progressRef }) {
       // Right side; visible clockwise rotation across frames
       {
         pos: [0.7, -0.74, -0.05],
-        rot: [0, Math.PI / 2, 0],
+        rot: [0, Math.PI / 2 - 0.85, 0],
         scale: 0.25,
       },
       {
         pos: [0.7, -0.74, -0.05],
-        rot: [0, Math.PI / 2, 0],
+        rot: [0, Math.PI / 2 - 0.85, 0],
         scale: 0.25,
       },
       {
         pos: [0.7, -0.74, -0.05],
-        rot: [0, Math.PI / 2, 0],
+        rot: [0, Math.PI / 2 - 0.85, 0],
         scale: 0.25,
       },
       {
         pos: [0.7, -0.74, -0.05],
-        rot: [0, Math.PI / 2, 0],
+        rot: [0, Math.PI / 2 - 0.85, 0],
         scale: 0.25,
       },
     ],
@@ -91,7 +91,7 @@ function Sculpture({ progressRef }) {
       ref={groupRef}
       scale={0.2}
       position={[-0.4, -0.65, 0.1]}
-      rotation={[0, Math.PI / 2, 0]}
+      rotation={[0, Math.PI / 2 - 0.85, 0]}
     >
       <primitive object={scene} />
     </group>
